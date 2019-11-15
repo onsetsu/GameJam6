8da8b76805e9cf4865a3d1303edc276f4b79784a
Markus-PC\Markus <markus.brand@student.hpi.de>
2019-11-15 18:00:05 +0100

    New scene changing

--- GameOneButton/Scripts/PlanningPhase.gd
+++ GameOneButton/Scripts/PlanningPhase.gd
@@ -11,6 +11,25 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().change_scene("res://Scenes/TestMap.tscn")
-		var tm = get_tree().get_root().get_node("TestMap")
-		print(tm)
+		var game_scene = load("res://Scenes/TestMap.tscn")
+		var executePhase = instance_into_root(game_scene)
+		print(executePhase)
+		remove_node(self)
+		#get_tree().change_scene("res://Scenes/TestMap.tscn")
+		#var tm = get_tree().get_root().get_node("TestMap")
+		#print(tm)
+
+func instance_into_root(scene_class):
+    var instance = scene_class.instance()
+    attach_to_root(instance)
+    return instance
+
+func attach_to_root(scene):
+    var root = get_tree().get_root()
+    root.add_child(scene)
+
+func remove_node(node):
+    var parent = node.get_parent()
+    if parent:
+        parent.remove_child(node)
+

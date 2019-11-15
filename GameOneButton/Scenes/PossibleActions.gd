1bb3ae8043c3156fe12c616f7603e8570dfdc7d2
Markus-PC\Markus <markus.brand@student.hpi.de>
2019-11-15 17:25:30 +0100

    Use InputManager in PlanningPhase and add clicked items to queue list

--- GameOneButton/Scenes/PossibleActions.gd
+++ GameOneButton/Scenes/PossibleActions.gd
@@ -3,11 +3,21 @@ extends ItemList
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var inputManager = get_node("../InputManager")
+onready var queuedActions = get_node("../QueuedActions")
+var actions
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.add_item("lol", null, true);
+	actions = inputManager.get_actions()
+	for action in actions:
+		self.add_item(action.id, null, true)
 
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func queue_action(index):
+	var selectedAction = actions[index]
+	queuedActions.queue(selectedAction)
--- GameOneButton/Scenes/QueuedActions.gd
+++ GameOneButton/Scenes/QueuedActions.gd
@@ -1,19 +1,15 @@
-extends Node
+extends ItemList
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const scene_path = "res://Actions.tscn"
-var actions_scene = preload(scene_path)
-onready var actions = actions_scene.instance()
-
-func get_actions():
-	return actions.get_children()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func queue(action):	
+	self.add_item(action.id, null, true);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass

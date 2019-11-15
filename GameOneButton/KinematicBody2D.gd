5df44c0e8c3c491ccf5369620f76903bcda72417
Storyxx <Storyxx@users.noreply.github.com>
2019-11-15 16:39:29 +0100

    Moved player + new player-controller

--- GameOneButton/KinematicBody2D.gd
+++ GameOneButton/KinematicBody2D.gd
@@ -1,22 +1,33 @@
 extends KinematicBody2D
 
-
-export (int) var speed = 200
+export (int) var run_speed = 100
+export (int) var jump_speed = -400
+export (int) var gravity = 1200
 
 var velocity = Vector2()
+var jumping = false
 
 func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('right'):
-        velocity.x += 1
-    if Input.is_action_pressed('left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+    velocity.x = 0
+    var right = Input.is_action_pressed('ui_right')
+    var left = Input.is_action_pressed('ui_left')
+    var jump = Input.is_action_pressed('ui_select')
+    
+    print(jump)
+
+    if jump and is_on_floor():
+        jumping = true
+        velocity.y = jump_speed
+    if jump and jumping:
+        velocity.y -= 400
+    if right:
+        velocity.x += run_speed
+    if left:
+        velocity.x -= run_speed
 
 func _physics_process(delta):
     get_input()
-    velocity = move_and_slide(velocity)
+    velocity.y += gravity * delta
+    if jumping and is_on_floor():
+        jumping = false
+    velocity = move_and_slide(velocity, Vector2(0, -1))

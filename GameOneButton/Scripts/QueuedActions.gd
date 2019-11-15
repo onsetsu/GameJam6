4d6b2c429ccb4b35bdd4252e3369c99fe866664a
Markus-PC\Markus <markus.brand@student.hpi.de>
2019-11-15 18:19:32 +0100

    stuff

--- GameOneButton/Scripts/QueuedActions.gd
+++ GameOneButton/Scripts/QueuedActions.gd
@@ -3,13 +3,19 @@ extends ItemList
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var queuedActions
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	queuedActions = []
 	pass # Replace with function body.
 
 func queue(action):	
+	queuedActions.push_back(action);
 	self.add_item(action.id, null, true);
+	
+func get_actions():
+	return queuedActions
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass

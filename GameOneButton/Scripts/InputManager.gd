5d4939662d8fdf02a7e29645e4093261b7b664d5
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2019-11-15 18:19:40 +0100

    Input logic

--- GameOneButton/Scripts/InputManager.gd
+++ GameOneButton/Scripts/InputManager.gd
@@ -4,13 +4,19 @@ extends Node
 # var a = 2
 # var b = "text"
 
-var actions
+var actions = []
+var index = 0
 
 func set_actions(actions):
 	self.actions = actions
 
 func _process(delta):
-	pass
+	if index >= actions.size(): return
+	if Input.is_action_just_pressed("ui_accept"):
+		Input.action_press(actions[index].id)
+	elif Input.is_action_just_released("ui_accept"):
+		Input.action_release(actions[index].id)
+		index += 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():

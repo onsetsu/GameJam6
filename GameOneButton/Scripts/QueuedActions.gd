5e50d6d735d381f785fdcbf88c17fdc0fbc6ad3f
Markus-PC\Markus <markus.brand@student.hpi.de>
2019-11-15 21:39:11 +0100

    Use Level Loading for action getting

--- GameOneButton/Scripts/QueuedActions.gd
+++ GameOneButton/Scripts/QueuedActions.gd
@@ -5,6 +5,10 @@ extends ItemList
 # var b = "text"
 var queuedActions
 
+const scene_path = "res://Actions.tscn"
+var actions_scene = preload(scene_path)
+onready var action_scene = actions_scene.instance().get_children()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.set_fixed_icon_size(Vector2(64, 64))
@@ -12,14 +16,21 @@ func _ready():
 	pass # Replace with function body.
 
 func queue(action):	
-	queuedActions.push_back(action);
 	self.add_item(action.display_name, action.image, true)
 	
 func get_actions():
-	return queuedActions
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var actions = []
+	for i in range(0, self.get_item_count()):
+		actions.push_back(_get_action_with_name(self.get_item_text(i)))
+		
+	return actions
+	
+
+func _get_action_with_name(name):
+	for action in action_scene:
+		if(action.display_name == name):
+			return action
+	return null
 
 
 func remove_action():

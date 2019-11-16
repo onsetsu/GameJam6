e06bafbfc510520e64b26715b86b43f7d2c0827f
Callista Gratz <callista@Callistas-MacBook-Pro.local>
2019-11-16 15:28:08 +0100

    Background

--- GameOneButton/Scripts/Background.gd
+++ GameOneButton/Scripts/Background.gd
@@ -6,4 +6,4 @@ onready var player = get_node("../Player")
 func _process(delta):	
 	# Scroll background
 	scroll_x -= 200 * delta
-	scroll_offset.x = player.position.x
+	scroll_offset.x = -player.position.x

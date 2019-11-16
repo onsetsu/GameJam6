b8d6760f5b5a3112981077892cc3ce8f16e4b708
Storyxx <rk81ogel@gmail.com>
2019-11-16 17:32:35 +0100

    jump animation again

--- GameOneButton/Scenes/AnimationPlayer.gd
+++ GameOneButton/Scenes/AnimationPlayer.gd
@@ -7,21 +7,21 @@ func _ready():
 	pass
 
 func _process(delta):
+	var mult = get_parent().gravity_multiplyer
 	old_vel_y = vel.y
-	vel = get_parent().get("velocity")
-	var trigger = get_parent().get("trigger")
-	var on_floor = get_parent().is_on_floor()
+	vel = get_parent().get("velocity")*mult
+	var on_floor = get_parent().was_grounded
 	if abs(vel.x) < 10 and on_floor:
 		stop()
 	elif not is_playing():
 		play("move_right")
 	
-	if vel.y < -10 and old_vel_y > -10:
+	if vel.y < 0 and old_vel_y >= 0:
 		get_node("../JumpingSprites").show()
 		get_node("../WalkingSprites").hide()
 		get_node("../FallingSprites").hide()
 		play("jump_up")
-	elif vel.y > 10 and sign(vel.y) != sign(old_vel_y):
+	elif vel.y > 0 and old_vel_y <= 0:
 		get_node("../FallingSprites").show()
 		get_node("../WalkingSprites").hide()
 		get_node("../JumpingSprites").hide()

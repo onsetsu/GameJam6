a9d23162be9163a098f4b1bd9cdad6fb96645e40
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2019-11-16 17:05:54 +0100

    Add SPlash

--- GameOneButton/Scenes/RootScene.gd
+++ GameOneButton/Scenes/RootScene.gd
@@ -5,11 +5,12 @@ extends Node2D
 # var b = "text"
 var first = true
 # Called when the node enters the scene tree for the first time.
-func _process(delta):
-	if(first):
-		first = false
-		LevelSingleton.loadScene()
+#func _process(delta):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_Timer_timeout():
+	LevelSingleton.loadScene()
+	remove_child($Splash)
